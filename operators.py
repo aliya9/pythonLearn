a65f904e2e3631954d0e8859f136f3bb3f27748c
Aliya <farisaliya2004@gmail.com>
2022-03-31 23:33:52 +0530

    Initial Commit

---
+++ operators.py
@@ -0,0 +1,70 @@
+# 1. Arithmetic Operators:- +,-,/,*,%,//,**
+
+
+a=2
+b=7
+c=b//a       # floor division(//)
+print(c)
+
+d= a+b
+print(d)
+
+e= a-b
+print(e)
+
+f= a*b
+print(f)
+
+g= a/b
+print(g)
+
+h= a%b
+print(h)
+
+i= a**2         #exponential operator
+print(i)
+
+# 2. Comparison Operators ( O/P is always boolean operator i.e. TRUE/FALSE ):- <,>,==,<=,>=,!=
+
+j= a<b
+print(j)
+
+k =a>b
+print(k)
+
+print(a==b)
+
+l= a<=b
+print(l)
+
+m= a>=b
+print(m)
+
+# 3. Membership Operators:- in, not in ( O/P is always boolean operator i.e. TRUE/FALSE )
+
+list= [10,20,30,40,50,60]
+
+print(22 in list)
+
+print(22 not in list)
+
+
+# 4. Identity Operators :- is, is not ( O/P is always boolean operator i.e. TRUE/FALSE )
+
+Number = 255
+
+print(125 is Number)
+
+print(125 is not Number)
+
+# 5. Boolean Operators:- True, Flase
+
+a=True
+print(a)
+
+b=False
+print(b)
+
+# 6. Assignment Operators :- =
+
+a=10
