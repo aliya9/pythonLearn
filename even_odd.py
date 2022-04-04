f2907ef5d2bbe34c6ce740fbee0718a1f7aa7e88
Aliya <farisaliya2004@gmail.com>
2022-04-04 20:05:59 +0530

    Initial Commit

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+a=int(input("Enter number: "))
+
+if (a%2==0):
+    print("Even number")
+else:
+    print("Odd number")
---
+++ if_else.py
@@ -0,0 +1,6 @@
+a=float(input("Enter your height: "))
+
+if a>4.5:
+    print("You're taller")
+else:
+    print("You're smaller")
