91e66651184bcd141ab6955f959acaa41860e390
Aliya <farisaliya2004@gmail.com>
2022-04-14 20:28:05 +0530

    Initial Commit

---
+++ pattern.py
@@ -0,0 +1,11 @@
+i=1
+
+for i in range(1,6):
+    print("*"*i)
+
+
+
+
+
+
+
---
+++ sq_even.py
@@ -0,0 +1,29 @@
+
+l1=[12,1,4,5,6,7,8,9]
+i=0
+# while True:
+#
+#     if i>len(l1):
+#         break
+#
+#     if l1[i] % 2 == 0:
+#         sq = l1[i] ** 2
+#         print(sq)
+#
+#     else:
+#         cb = l1[i] ** 3
+#         print(cb)
+#
+#     i += 1
+
+while i<len(l1):
+
+    if l1[i]%2 == 0:
+        sq = l1[i] **2
+        print(sq)
+    else:
+        cb = l1[i] **3
+        print(cb)
+
+    i +=1
+
