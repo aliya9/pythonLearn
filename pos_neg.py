f728d73c01fa7a211f1eb7b95f9fa35d948a8ff3
Aliya <farisaliya2004@gmail.com>
2022-04-14 21:21:47 +0530

    Initial Commit

---
+++ pos_neg.py
@@ -0,0 +1,19 @@
+i = 0
+l1 = [-10, -8, 5, 34, 3, 8, -9, -67]
+pos = 0
+neg = 0
+
+while i < len(l1):
+
+    if l1[i] > 0:
+        pos += 1
+
+
+    else:
+        neg += 1
+
+    i += 1
+
+
+print("Positive numbers: " +str(pos))
+print("Negative numbers: " +str(neg))
