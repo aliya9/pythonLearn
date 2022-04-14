4f2e7737a6c13a6b0a3910c8092ce22b50a7cee6
Aliya <farisaliya2004@gmail.com>
2022-04-14 20:30:21 +0530

    Initial Commit

--- sq_even.py
+++ sq_even.py
@@ -1,6 +1,6 @@
 
-l1=[12,1,4,5,6,7,8,9]
-i=0
+l1 = [12, 1, 4, 5, 6, 7, 8, 9]
+i = 0
 # while True:
 #
 #     if i>len(l1):
@@ -16,14 +16,13 @@ i=0
 #
 #     i += 1
 
-while i<len(l1):
+while i < len(l1):
 
-    if l1[i]%2 == 0:
-        sq = l1[i] **2
+    if l1[i] % 2 == 0:
+        sq = l1[i] ** 2
         print(sq)
     else:
-        cb = l1[i] **3
+        cb = l1[i] ** 3
         print(cb)
 
-    i +=1
-
+    i += 1
